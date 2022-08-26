4aafe5c961841b3c565347163febe782ec0afce1
thales-bpl <thales.bplima@gmail.com>
2022-08-26 04:19:05 -0300

    impl: req16

---
+++ challenges/desafio16.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $set: { ultimaModificacao: Date.now() } },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { _id: 0, nome: true },
+);
